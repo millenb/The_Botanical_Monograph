8d93282311c34be356acbee27972dff822e9e72d
millenb <bmillen@gradcenter.cuny.edu>
2022-09-30 14:18:47 -0400

    Added initial pop-ups to all pages

--- myScript.js
+++ myScript.js
@@ -1,5 +1,20 @@
 // When the user clicks on <div>, open the pop-up
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+function myFunction1() {
+    var popup1 = document.getElementById("myPopup1");
+    popup1.classList.toggle("show");
+}
+
+function myFunction2() {
+    var popup2 = document.getElementById("myPopup2");
+    popup2.classList.toggle("show");
+}
+
+function myFunction3() {
+    var popup3 = document.getElementById("myPopup3");
+    popup3.classList.toggle("show");
+}
+
+function myFunction4() {
+    var popup4 = document.getElementById("myPopup4");
+    popup4.classList.toggle("show");
 }
